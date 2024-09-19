2ff05096a1644369a60bebb9bbf0efb30d0a1d97
Damien MAILHEBIAU <damienmailhebiau@gmail.com>
2024-09-19 15:08:09 +0200

    ajout du modele devise

---
+++ WSConvertisseur/Models/Devise.cs
@@ -0,0 +1,42 @@
+﻿namespace WSConvertisseur.Models
+{
+    public class Devise
+    {
+		private int id;
+
+		public int Id
+		{
+			get { return id; }
+			set { id = value; }
+		}
+
+		private string? nomDevise;
+
+		public string? NomDevise
+		{
+			get { return nomDevise; }
+			set { nomDevise = value; }
+		}
+
+		private double taux;
+
+		public double Taux
+		{
+			get { return taux; }
+			set { taux = value; }
+		}
+
+        public Devise(int id, string nomDevise, double taux)
+        {
+            this.id = id;
+            this.nomDevise = nomDevise;
+            this.taux = taux;
+        }
+
+        public Devise()
+        {
+            
+        }
+
+    }
+}
