2c6502cc8415a1c8dff0349a6d3b1829570d8b32
Damien MAILHEBIAU <damienmailhebiau@gmail.com>
2024-09-20 17:16:35 +0200

    refactor directories, init tp2

---
+++ tp2/TP2Console/TP2Console/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace TP2Console
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
