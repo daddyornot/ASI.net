0a6458415d0c43078a6869640202326652c89f3b
Damien MAILHEBIAU <damienmailhebiau@gmail.com>
2024-09-20 11:16:07 +0200

    v2 api call works

---
+++ ClientConvertisseurV2/ClientConvertisseurV2-2/Services/IServices.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using ClientConvertisseurV2_2.Models;
+
+namespace ClientConvertisseurV2_2.Services
+{
+    internal interface IServices
+    {
+        Task<List<Devise>> GetDevisesAsync(string nomControleur);
+    }
+}
---
+++ ClientConvertisseurV2/ClientConvertisseurV2-2/ViewModels/ConvertisseurEuroViewModel.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.ObjectModel;
+using ClientConvertisseurV2_2.Models;
+using ClientConvertisseurV2_2.Services;
+using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace ClientConvertisseurV2_2.ViewModels;
+
+public class ConvertisseurEuroViewModel: ObservableObject
+{
+    private ObservableCollection<Devise> devises;
+    private WSServices service;
+
+    public ConvertisseurEuroViewModel()
+    {
+        service = WSServices.GetInstance("http://localhost:5179/api/");
+        ActionGetAsyncData();
+    }
+
+    private async void ActionGetAsyncData()
+    {
+        var result = await service.GetDevisesAsync("devises");
+        Devises = new ObservableCollection<Devise>(result);
+    }
+
+    public ObservableCollection<Devise> Devises
+   {
+      get { return devises; }
+      set
+      {
+         devises = value;
+         OnPropertyChanged(); // Notify the UI that the property has changed
+      }
+   }
+}
